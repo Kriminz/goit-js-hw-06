17e185c09c140c3520f34d425274863217148438
Kriminz <artur26062004@gmail.com>
2023-02-23 16:26:34 +0200

    Друге завдання

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.querySelector(`#ingredients`)
+
+for(const i of ingredients){
+  let elem = document.createElement("li");
+  elem.textContent = i;
+  listEl.append(elem);
+}
