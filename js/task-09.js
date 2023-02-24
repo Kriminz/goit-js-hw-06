fd8d38bbeb7cf96396a0cf3f016c70acc2c842bd
Kriminz <artur26062004@gmail.com>
2023-02-24 18:30:29 +0200

    Дев'яте завдання

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const body = document.querySelector('body');
+const colorName = document.querySelector('.color');
+const changeButton = document.querySelector('.change-color');
+
+changeButton.addEventListener('click', checkColorChange);
+
+function checkColorChange(event) {
+  body.style.backgroundColor = getRandomHexColor();
+  colorName.textContent = getRandomHexColor();
 }
