130aa0b8e4cd00fd9c3f60fb7c48e0781bce0f90
Kriminz <artur26062004@gmail.com>
2023-02-24 18:09:33 +0200

    Восьме завдання

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector(".login-form");
 
+const alarm = document.createElement("p");
+
+function checkForm(event) {
+  event.preventDefault();
+
+  const { elements: { email, password }, } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alarm.textContent = "Введіть пошту та пароль!";
+    form.after(alarm);
+    console.log(alarm);
+    return alarm;
+  }
+
+
+  console.log(`email: ${email.value}, Password: ${password.value}`);
+
+  event.currentTarget.reset();
+}
+
+// alarm.remove();
+
+form.addEventListener("submit", checkForm);
